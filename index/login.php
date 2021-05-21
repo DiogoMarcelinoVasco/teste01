1264fe93de0dc42635fcfe4801d181eee25f5893
DiogoMarcelinoVasco <diogomv59@gmail.com>
2021-05-21 11:50:14 -0300

    Update login.php

--- index/login.php
+++ index/login.php
@@ -1,7 +1,7 @@
 <?php
 $usuario = $_POST["usuario"];
 $senha = $_POST["senha"];
-if($senha == "10" && $usuario == "10"){
-    echo "10";}
+if($senha == "12" && $usuario == "12"){
+    echo "12";}
 ?>
 
