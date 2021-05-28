2eacbccdecfc0334db5dd4c14ff2a24231909233
DiogoMarcelinoVasco <diogomv59@gmail.com>
2021-05-28 20:01:24 -0300

    d

--- teste02/Clientes.php
+++ teste02/Clientes.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <title>Clientes</title>
-  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="css/style.css">
   
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
